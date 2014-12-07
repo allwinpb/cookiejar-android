bf7910ad07fc9f86e914d50e1041e6aaea745296
Allwin Panikulam <allwinpb@gmail.com>
2014-12-07 09:42:30 +0800

    Things

--- cookiejar/www/js/cookiejar.js
+++ cookiejar/www/js/cookiejar.js
@@ -1,2 +1,47 @@
 var CookieJarClient = function(){
+	$.support.cors = true
+	this.hostname = "cookiejar-hackatron.herokuapp.com"
+	this.interval = 5
+	this.completedTasksCallback = function(){}
+	this.pendingTasksCallback = function(){}
+	this.pendingTaskDaemon = function(){
+		$.ajax({
+			url: this.hostname + "/users/1/tasks?filter=pending",
+			method: "get",
+			success: function(data){
+				console.log("Received Pending Tasks")
+				this.pendingTasksCallback(data)
+			},
+			error: function(){}
+		});
+	}
+	this.completedTaskDaemon = function(){
+		$.ajax({
+			url: this.hostname + "/users/1/tasks?filter=completed",
+			method: "get",
+			success: function(data){
+				console.log("Received completed tasks")
+				this.completedTasksCallback(data)
+			},
+			error: function(){}
+		});
+	}
+	this.onReceivingPendingTasks = function(callback){
+		this.pendingTasksCallback = callback
+	}
+	this.onReceivingCompletedTasks = function(callback){
+		this.completedTasksCallback = callback
+	}
+	this.completeTask = function(task){
+		$.ajax({
+			method: "post",
+			url: this.hostname + "/users/1/tasks/" + task + "/complete",
+			error: function(data){
+				console.log("ERROR: " + data)
+			}
+		});
+	}
+
+	setInterval(this.pendingTaskDaemon, this.interval)
+	setInterval(this.completedTaskDaemon, this.interval)
 }
